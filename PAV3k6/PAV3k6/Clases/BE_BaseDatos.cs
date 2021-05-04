e09857d3733b6b85b2f31c42cf89636eedcd0bd7
Santiago Emanuel Salas Canalicchio <83949@sistemas.frc.utn.edu.ar>
2021-05-03 22:51:32 -0300

    Agrega la base de datos

---
+++ PAV3k6/PAV3k6/Clases/BE_BaseDatos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PAV3k6.Clases
+{
+    class BE_BaseDatos
+    {
+        SqlConnection Conexion = new SqlConnection();
+        SqlCommand Cmd = new SqlCommand();
+        string Cadena_conexion = "";
+
+        private void Conectar()
+        {
+            Conexion.ConnectionString = Cadena_conexion;
+            Conexion.Open();
+            Cmd.Connection = Conexion;
+            Cmd.CommandType = System.Data.CommandType.Text;
+        }
+
+        private void Desconectar()
+        {
+            Conexion.Close();
+        }
+
+        public DataTable Consulta(string Sql)
+        {
+            Conectar();
+            Cmd.CommandText = Sql;
+            DataTable tabla = new DataTable();
+            tabla.Load(Cmd.ExecuteReader());
+            Desconectar();
+            return tabla;
+        }
+
+
+
+    }
+}
