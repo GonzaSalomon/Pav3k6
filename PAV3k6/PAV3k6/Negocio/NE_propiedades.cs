0fd022f2ffa0a49aa19d8586e315e791f147ad26
Santiago Emanuel Salas Canalicchio <83949@sistemas.frc.utn.edu.ar>
2021-05-04 16:36:14 -0300

    asd

---
+++ PAV3k6/PAV3k6/Negocio/NE_propiedades.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using PAV3k6.Clases;
+
+
+namespace PAV3k6.Negocio
+{
+    class NE_Propiedades
+    {
+        BE_BaseDatos _BD = new BE_BaseDatos();
+
+        public DataTable RecuperarTodos()
+        {
+            string sql = "SELECT * FROM propiedades";
+            return _BD.Consulta(sql);
+        }
+
+        public DataTable RecuperarDesignacion(int designacion)
+        {
+            string sql = "SELECT * FROM propiedades WHERE designacion_catastral =" + designacion.ToString();
+            return _BD.Consulta(sql);
+        }
+    }
+}
