a08a64a7c09f3e70f42f2bd2f9eb093b8bbd000d
Gonzalo Salomón <54727856+GonzaSalomon@users.noreply.github.com>
2021-05-09 16:52:08 -0300

    Update Program.cs

--- PAV3k6/PAV3k6/Program.cs
+++ PAV3k6/PAV3k6/Program.cs
@@ -15,10 +15,9 @@ namespace PAV3k6
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            ///Application.Run(new Form1());
-            Application.Run(new ABMC_Barrio());
+            Application.Run(new Frm_AMB_Propiedades());
+            //Application.Run(new FRM_Alta_Propiedades());
+            //Application.Run(new Frm_Modificar_Propiedades());
         }
     }
 }
