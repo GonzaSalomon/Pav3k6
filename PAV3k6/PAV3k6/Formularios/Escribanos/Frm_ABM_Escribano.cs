3ad685dbd3cf367ec49486460dd41ab7a86e76c0
vairo-321 <54008591+vairo-321@users.noreply.github.com>
2021-05-11 02:20:08 -0300

    ABM_Escribanos

---
+++ PAV3k6/PAV3k6/Formularios/Escribanos/Frm_ABM_Escribano.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAV3k6.Formularios.Escribanos
+{
+    public partial class Frm_ABM_Escribano : Form
+    {
+        public Frm_ABM_Escribano()
+        {
+            InitializeComponent();
+        }
+    }
+}
