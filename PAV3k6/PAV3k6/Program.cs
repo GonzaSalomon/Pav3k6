e93851f43f7750158e638d60ce8f872eefc16be4
Masi <abcd123@gmail.com>
2021-05-11 00:47:34 -0300

    llborrar

--- PAV3k6/PAV3k6/Program.cs
+++ PAV3k6/PAV3k6/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using PAV3k6.Formularios;
 
 namespace PAV3k6
 {
